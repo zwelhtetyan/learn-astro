0ac743b182d2c4b5168b5e1abf66d2d13bcecf30
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-10-07 14:57:21 +0630

    feat: greeeting with preact and theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://learn-astro-pink.vercel.app"
+  site: "https://learn-astro-pink.vercel.app",
+  integrations: [preact()]
 });
-
-
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'preact/hooks';
+
+interface Props {
+  messages: string[]
+}
+
+export default function Greeting({messages}: Props) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  );
+}
